a2af3e0316aaa310b84b09fb71c02c93763f43ac
vietduc-it <levietduc4566@gmail.com>
2024-04-24 13:05:18 +0700

    Add project files.

---
+++ Viettel/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Viettel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(766, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start / Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(599, 45);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(150, 22);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(904, 472);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 36);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(112, 39);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 573);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Viettel/Form1.cs
@@ -0,0 +1,118 @@
+﻿//using DocumentFormat.OpenXml.Drawing.Charts;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Viettel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        [DllImport("user32.dll")]
+
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwdata, int dwextrainfo);
+
+        public enum mouseeventflags
+        {
+            // Bấm nút
+            LeftDown = 2,
+            // Thả nút
+            LeftUp = 4,
+        }
+
+        public void leftclick(Point p)
+        {
+            mouse_event((int)(mouseeventflags.LeftDown), p.X, p.Y, 0, 0);
+            mouse_event((int)(mouseeventflags.LeftUp), p.X, p.Y, 0, 0);
+        }
+
+        bool stop = true;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stop =  (stop) ? false : true;
+            timer1.Interval = (int)numericUpDown1.Value;
+            timer1.Enabled = true;
+
+            if (!stop) timer1.Start();
+            if(stop) timer1.Stop();
+            
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            // Kiểm tra nếu không có hàng nào trong DataGridView
+            if (dataGridView1.Rows.Count == 0)
+                return;
+
+            // Lấy chỉ mục của hàng hiện tại (index của hàng đầu tiên)
+            int currentRowIndex = dataGridView1.FirstDisplayedCell.RowIndex;
+  
+
+            // Kiểm tra xem chỉ mục hàng hiện tại có lớn hơn hoặc bằng số hàng trong DataGridView không
+            if (currentRowIndex >= dataGridView1.Rows.Count)
+                return;
+
+            // Lấy giá trị từ ô trong hàng hiện tại
+            string cellValue = dataGridView1.Rows[currentRowIndex].Cells[5].Value?.ToString(); // Lưu ý sử dụng ?. để kiểm tra null trước khi gọi ToString()
+
+            // Kiểm tra xem giá trị có rỗng không
+            if (string.IsNullOrEmpty(cellValue))
+                return;
+
+            // Gửi giá trị của ô đến ứng dụng đích
+            SendKeys.SendWait(cellValue);
+
+            // Gửi phím Enter
+            SendKeys.SendWait("{ENTER}");
+
+            // Dịch chuyển sang hàng tiếp theo trong DataGridView
+            dataGridView1.FirstDisplayedCell = dataGridView1.Rows[currentRowIndex + 1].Cells[0];
+        }
+
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dilg = new OpenFileDialog();
+            dilg.Filter = "Excel Sheet(*.xlsx)|*.xlsx|All Files(*.*)|*.*";
+            if (dilg.ShowDialog() == DialogResult.OK)
+            {
+                string filepath = dilg.FileName;
+                textBox1.Text = filepath;
+
+                LoadDataFromExceltoDataGridView(filepath, ".xlsx", "yes");
+            }
+        }
+
+
+        public void LoadDataFromExceltoDataGridView(string fpath, string ext, string hdr)
+        {
+            string con = "Provider = Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
+            con = string.Format(con, fpath, "yes");
+            OleDbConnection excelconnection = new OleDbConnection(con);
+            excelconnection.Open();
+            DataTable dtexcel = excelconnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            string excelsheet = dtexcel.Rows[0]["TABLE_NAME"].ToString();
+            OleDbCommand com = new OleDbCommand("Select * from [" + excelsheet + "]", excelconnection);
+            OleDbDataAdapter oda = new OleDbDataAdapter(com);
+            DataTable dt = new DataTable();
+            oda.Fill(dt);
+            excelconnection.Close();
+
+            dataGridView1.DataSource = dt;
+        }
+
+
+    }
+}
